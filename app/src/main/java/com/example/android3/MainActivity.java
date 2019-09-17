2f53b7211ea09ccd9c0ea929e655952626bb477f
AndreevIlya <katauri@yandex.ru>
2019-09-17 21:50:17 +0300

    Input is made transferable into the model.

--- app/src/main/java/com/example/android3/MainActivity.java
+++ app/src/main/java/com/example/android3/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.android3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.example.android3.presenter.Presenter;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectPresenter
+    Presenter presenter;
+
+    private EditText input;
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = findViewById(R.id.input);
+        output = findViewById(R.id.output);
+
+        initListeners();
+    }
+
+    private void initListeners() {
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.i("TextWatcher","before");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.i("TextWatcher","on");
+                presenter.processTextInput(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.i("TextWatcher","after");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android3/presenter/Presenter.java
+++ app/src/main/java/com/example/android3/presenter/Presenter.java
@@ -1,7 +1,32 @@
 package com.example.android3.presenter;
 
+import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
+import com.example.android3.model.Model;
 import com.example.android3.view.MoxyView;
 
+@InjectViewState
 public class Presenter extends MvpPresenter<MoxyView> {
+
+    private Model model;
+
+    @Override
+    protected void onFirstViewAttach() {
+        super.onFirstViewAttach();
+        model = new Model();
+    }
+
+    @Override
+    public void attachView(MoxyView view) {
+        super.attachView(view);
+    }
+
+    @Override
+    public void detachView(MoxyView view) {
+        super.detachView(view);
+    }
+
+    public void processTextInput(String text){
+        model.addToContent(text);
+    }
 }
